20c03f59f1de615f8f8d8187e923a40cd2d8ea3d
oleksandr.vasyliev <oleksandr.vasyliev@isoftbet.com>
2022-02-12 14:46:26 +0200

    chg: Finish table resize

--- src/core/dom.js
+++ src/core/dom.js
@@ -38,6 +38,33 @@ class Dom {
     }
     return this
   }
+
+  get data() {
+    return this.$el.dataset
+  }
+  closest(selector) {
+    return $(this.$el.closest(selector));
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect();
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector)
+  }
+  /*
+  * {
+  *   heigth:
+  *   width:
+  *   backgroundColor:
+  * }
+  * */
+  css(styles = {}) {
+    Object.keys(styles).forEach(key =>{
+      this.$el.style[key] = styles[key]
+    })
+  }
 }
 
 $('div').html('<h1>Test</h1>').clear()
